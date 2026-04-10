980cd395a1f80ba6b7a4caff16103fb4854a03d1
Андрей <Kovalc678@gmail.com>
2026-04-10 12:46:55 +0300

    Add: class for reading data

---
+++ src/readers/DataReader.py
@@ -0,0 +1,11 @@
+import os
+from abc import ABC, abstractmethod
+from typing import List
+from models import Country
+
+# Інтерфейс для зчитування даних
+class DataReader(ABC):
+    @abstractmethod
+    def read_data(self, file_path: str) -> List[Country]:
+        """Абстрактний метод для зчитування даних з файлу."""
+        pass
