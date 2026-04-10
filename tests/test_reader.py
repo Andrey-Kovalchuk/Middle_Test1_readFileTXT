8c79f958dca8cffd6751c9cd63579ecbda8242c7
Андрей <Kovalc678@gmail.com>
2026-04-10 13:16:58 +0300

    Add: class for test velibal data

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -7,3 +7,10 @@ def temp_data_file(tmp_path):
     file_path = tmp_path / "test_data.txt"
     file_path.write_text("США, 9833517, 331000000\nКанада, 9984670, 37742154", encoding="utf-8")
     return str(file_path)
+def test_read_valid_data(temp_data_file):
+    # Перевірка методу read_data на коректних даних
+    reader = TextFileReader()
+    countries = reader.read_data(temp_data_file)
+    assert len(countries) == 2
+    assert countries[0].name == "США"
+    assert countries[1].population == 37742154
