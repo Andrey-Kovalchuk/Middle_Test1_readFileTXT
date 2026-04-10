c7af7d097e0babca106d172d70e3e4570516b7b9
Андрей <Kovalc678@gmail.com>
2026-04-10 12:57:12 +0300

    Add: work with users

--- src/CountryApp.py
+++ src/CountryApp.py
@@ -9,3 +9,13 @@ class CountryApp:
         """Ін'єкція залежностей через конструктор."""
         self.reader = reader
         self.sorter = sorter
+    def process(self, file_path: str) -> Tuple[List[Country], List[Country]]:
+        """
+        Основний метод, який зчитує дані та повертає два відсортовані списки.
+        """
+        countries = self.reader.read_data(file_path)
+        
+        sorted_by_area = self.sorter.sort_by_area(countries, reverse=True)
+        sorted_by_population = self.sorter.sort_by_population(countries, reverse=True)
+        
+        return sorted_by_area, sorted_by_population
